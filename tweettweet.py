2831b6429fbb6feae0675dcabe4294765cfe6b73
kc <krishnasai.chivukula@gmail.com>
2020-04-04 17:22:46 +0100

    intial commit

--- tweettweet.py
+++ tweettweet.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler('FVLo08sCQXkS5onU4plYp5pEx', 'Uo1teC7Hm7xtU1yBv7PbsT0u004CHTxVFppzDfMlRH9jGDC3Jl')
+auth.set_access_token('1246459420197949440-PfQN8kfz40Ni8S8k2aftQUOvGh9al3', 'c0hPTtndB04CnAejqAMQ8Ufj6hJ8mRL5NY45JNAxA9uxD')
 
 api = tweepy.API(auth)
 
