f945991b2b92d959d4cf0f24dd7e89d56c0538e7
kc <krishnasai.chivukula@gmail.com>
2020-04-04 17:47:42 +0100

    included

--- tweettweet.py
+++ tweettweet.py
@@ -1,10 +1,19 @@
 import tweepy
-
+import time
 auth = tweepy.OAuthHandler('FVLo08sCQXkS5onU4plYp5pEx', 'Uo1teC7Hm7xtU1yBv7PbsT0u004CHTxVFppzDfMlRH9jGDC3Jl')
 auth.set_access_token('1246459420197949440-PfQN8kfz40Ni8S8k2aftQUOvGh9al3', 'c0hPTtndB04CnAejqAMQ8Ufj6hJ8mRL5NY45JNAxA9uxD')
 
 api = tweepy.API(auth)
+user = api.me()
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def limit_handel(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(300)
+
+
+for follower in limit_handel(tweepy.Cursor(api.followers).items()):
+    follower.follow()
